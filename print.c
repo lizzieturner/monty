cedc81617f02ec58fe60c0996556de8e27b2fcbb
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-04-24 22:27:39 +0000

    adding two files for advances features, transform and print

---
+++ print.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * _pall - prints all the values on the stack starting from the top
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void _pall(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+        UNUSED(line_number);
+}
+
+/**
+ * _pint - prints the value at the top of the stack, followed by a new line
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void _pint(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+        UNUSED(line_number);
+}
+
+/**
+ * _pchar - prints the char at the top of the stack, followd by a new line
+ * @stack: stack
+ * @line_number: line_number
+ */
+
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+        UNUSED(line_number);
+}
+
+/**
+ * _pstr - prints the string starting at the top of the stack, followed
+ * by a new line
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void _pstr(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+	UNUSED(line_number);
+}
---
+++ transform.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * _swap - swaps the top two elements of the stack
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+        UNUSED(line_number);
+}
+
+/**
+ * _rotl - rotates the stack to the top
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void _rotl(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+        UNUSED(line_number);
+}
+
+/**
+ * _rotr - prints the char at the top of the stack, followed 
+ * by a new line
+ * @stack: stack
+ * @line_number: line_number
+ */
+
+void _rotr(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(stack);
+        UNUSED(line_number);
+}
